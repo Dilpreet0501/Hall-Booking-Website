066460cbc1d7974bd7de2b6219fb8390afec1597
Avyukt <avyuktrkumar08@gmail.com>
2023-10-28 03:31:26 +0530

    ui changes

--- src/pages/Form.js
+++ src/pages/Form.js
@@ -8,7 +8,7 @@ function Form(props) {
  
 
   return (
-    <div>
+    <div className='form_class'>
       <form className="f1">
         <label className="heading">Name</label>
         <input type="text" name="name" className="entry"/>
@@ -20,15 +20,15 @@ function Form(props) {
         <input type="text" className="entry"/>
 
         <label className="heading">Select available date and time slots</label>
-        <button type="submit"><Link to="/booking" ><span className='linkcol'>Click to open booking calendar</span></Link></button>
+        <button type="submit"><Link to="/booking" ><div className='linkcol'>Click to open booking calendar</div></Link></button>
         
-        <label className="disp_date"> Date</label>
+        <div className="disp_date"> Date</div>
         <input type="text" name="datedisp" className="date_final" value={JSON.parse(localStorage.getItem('date'))} />
       <br></br>
-        <label className="disp_time">Time</label>
+        <div className="disp_time">Time</div>
         <input type="text" className="time_final" value={JSON.parse(localStorage.getItem('time'))} />
        <br></br>
-        <label className='heading'>Email</label>
+        <div className='heading' style={{textAlign:'left'}}>Email</div>
         <input type='email' name="user_email" className='entry'/>
 
         <button className="submit" >Request Booking</button>
